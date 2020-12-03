ff985db5f0ca0d910ebda5a61082487079a5578d
adamkhairi <khairiadam2@gmail.com>
2020-12-03 20:30:34 +0100

    TVA calcul need fixes

--- firstBrief/Program.cs
+++ firstBrief/Program.cs
@@ -6,14 +6,14 @@ namespace firstBrief
     {
         private static void Main() //Point d'entrer.
         {
-            // Console.WriteLine("Khairi Adam");
-            // Collections.collc();
-            // Collections.browse();
+             Console.WriteLine("Khairi Adam");
+             Collections.collc();
+             Collections.browse();
             // Collections.clone();
             // Collections.insertList();
             // Collections.changeIn();
             // Collections.delete(3);
-            Loops.boucles();
+            // Loops.boucles();
 
         }
 
